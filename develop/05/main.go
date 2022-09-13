5059e4fd47cfcd2966b42b82e8d143b95b2a3b59
cmetallo42 <denerohin@yandex.ru>
2022-09-14 01:00:09 +0300

    init

--- develop/05/main.go
+++ develop/05/main.go
@@ -9,66 +9,124 @@ import (
 
 func main() {
 	var files []string
-	path := "examples"
+	flag := false
+	var pathPlusFile string
+	path := "examples/"
 	var substr string
-	keys := make(map[string]struct{})
+	keys := make(map[rune]int)
 	sliceOfStrings := make([]string, 0, 1)
 
-	if len(os.Args) > 1 {
-		substr = os.Args[1]
+	for i, arg := range os.Args {
+		if arg[0] == '-' {
+			for _, r := range arg {
+				if r == 'A' || r == 'B' || r == 'C' {
+					nextI, err := strconv.Atoi(os.Args[i+1])
+					if err != nil {
+						panic(err)
+					}
+					keys[r] = nextI
+					flag = true
+				} else {
+					keys[r] = 0
+				}
+			}
+		} else if i < len(os.Args)-1 && i > 0 && arg != "examples" && arg != "main.go" {
+			if !flag {
+				if substr == "" {
+					substr += arg
+				} else {
+					substr += " " + arg
+				}
+			} else {
+				flag = false
+			}
+		}
 	}
+
 	if substr == "" {
 		fmt.Println("No Substring")
 		return
 	}
 
-	for _, args := range os.Args {
-		if args[0] == '-' {
-			for _, r := range args {
-				keys[string(r)] = struct{}{}
-			}
-		}
-	}
-	delete(keys, "-")
-	//    _, keyA := keys["A"]
-	// _, keyB := keys["B"]
-	// _, keyC := keys["C"]
-	// _, keyc := keys["c"]
-	// _, keyi := keys["i"]
-	// _, keyv := keys["v"]
-	// _, keyF := keys["F"]
-	_, keyn := keys["n"]
+	delete(keys, '-')
+	_, keyA := keys['A']
+	_, keyB := keys['B']
+	_, keyC := keys['C']
+	// _, keyc := keys['c']
+	// _, keyi := keys['i']
+	// _, keyv := keys['v']
+	// _, keyF := keys['F']
+	_, keyn := keys['n']
 
-	f, err := os.Open(path)
-	if err != nil {
-		panic(err)
-	}
-	fileInfo, err := f.Readdir(-1)
-	f.Close()
-	if err != nil {
-		panic(err)
-	}
-
-	for _, file := range fileInfo {
-		files = append(files, file.Name())
-	}
-
-	for _, file := range files {
-		tmp, err := os.ReadFile(path + "/" + file)
+	if strings.Contains(os.Args[len(os.Args)-1], "main.go") || strings.Contains(os.Args[len(os.Args)-1], "*.txt") {
+		f, err := os.Open(path)
 		if err != nil {
 			panic(err)
 		}
-		tmpStr := strings.Split(string(tmp), "\n")
-		for i, t := range tmpStr {
-			iStr := strconv.Itoa(i + 1)
-			if keyn && strings.Contains(string(t), substr) {
-				sliceOfStrings = append(sliceOfStrings, path+"/"+file+":"+iStr+":"+string(t)+"\n")
-			} else if !keyn && strings.Contains(string(t), substr) {
-				sliceOfStrings = append(sliceOfStrings, string(t)+"\n")
-			}
+		fileInfo, err := f.Readdir(-1)
+		f.Close()
+		if err != nil {
+			panic(err)
 		}
 
-	}
+		for _, file := range fileInfo {
+			files = append(files, file.Name())
+		}
 
-	fmt.Println(sliceOfStrings)
+		for _, file := range files {
+			tmp, err := os.ReadFile(path + file)
+			if err != nil {
+				panic(err)
+			}
+			pathPlusFile = path + file + ":"
+			tmpStr := strings.Split(string(tmp), "\n")
+			for i, t := range tmpStr {
+				iStr := strconv.Itoa(i + 1)
+				if strings.Contains(t, substr) {
+					if keyn {
+						pathPlusFile = path + file + ":" + iStr + ":"
+					}
+					if keyA {
+						aInt := keys['A']
+						for j := 0; j <= aInt; j++ {
+							if i+j > len(tmpStr)-1 {
+								break
+							}
+							if keyn {
+								pathPlusFile = path + file + ":" + strconv.Itoa(i+j) + ":"
+							}
+							sliceOfStrings = append(sliceOfStrings, pathPlusFile+tmpStr[i+j])
+						}
+					}
+					if keyB {
+						bInt := keys['B']
+						for j := bInt; j >= 0; j-- {
+							if i-j < 0 {
+								break
+							}
+							if keyn {
+								pathPlusFile = path + file + ":" + strconv.Itoa(i-j) + ":"
+							}
+							sliceOfStrings = append(sliceOfStrings, pathPlusFile+tmpStr[i-j])
+						}
+					}
+					if keyC {
+						cInt := keys['C']
+						for j := 0 - cInt; j <= cInt; j++ {
+							if i+j < 0 || i+j > len(tmpStr)-1 {
+								break
+							}
+							if keyn {
+								pathPlusFile = path + file + ":" + strconv.Itoa(i+j) + ":"
+							}
+							sliceOfStrings = append(sliceOfStrings, pathPlusFile+tmpStr[i+j])
+						}
+					}
+				}
+			}
+		}
+	}
+	for _, s := range sliceOfStrings {
+		fmt.Println(s)
+	}
 }
